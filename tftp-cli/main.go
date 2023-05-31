5c929350d564eabc54d31cc070baab9321ee2b8c
thc <thc2cat@gmail.com>
2023-05-31 15:18:29 +0200

    v0.1 Working Server and CLI

---
+++ tftp-cli/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	"github.com/pin/tftp/v3"
+)
+
+// tftp "github.com/pin/tftp/v3"
+var (
+	myLogger = log.New(os.Stderr, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+)
+
+func main() {
+
+	var (
+		put, get                      bool
+		localFile, Server, remoteFile string
+		blocksize                     int
+	)
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "[options] [hosts...]")
+		flag.PrintDefaults()
+	}
+
+	flag.BoolVar(&put, "put", false, "Send")
+	flag.BoolVar(&get, "get", false, "Get")
+
+	flag.StringVar(&localFile, "l", "", "local file")
+	flag.StringVar(&remoteFile, "r", "", "remote filename")
+	flag.StringVar(&Server, "s", "", "tftp server address")
+	flag.IntVar(&blocksize, "blocksize", 8*1024, "blocksize")
+
+	flag.Parse()
+
+	c, err := tftp.NewClient(Server)
+	if err != nil {
+		myLogger.Fatal(err)
+	}
+	c.SetBlockSize(blocksize)
+
+	switch {
+	case put: // UPLOAD :
+		file, err := os.Open(localFile)
+		if err != nil {
+			myLogger.Fatal(err)
+		}
+		ts := time.Now()
+
+		// c.SetTimeout(3 * time.Second) // optional
+		rf, err := c.Send(remoteFile, "octet")
+		if err != nil {
+			myLogger.Fatal(err)
+		}
+		n, err := rf.ReadFrom(file)
+		if err != nil {
+			myLogger.Fatal(err)
+		}
+		file.Close()
+		d := time.Now().Sub(ts)
+
+		fmt.Printf("PUT %s as %s/%s : %d bytes sent at %s/s\n",
+			localFile, Server, remoteFile,
+			n, prettyByteSize(float64(n)/(d.Seconds())))
+	case get: // DOWNLOAD:
+		// c.RequestTSize(true)
+		ts := time.Now()
+		wt, err := c.Receive(remoteFile, "octet")
+		if err != nil {
+			log.Fatal(err)
+		}
+		file, err := os.Create(localFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Optionally obtain transfer size before actual data.
+		// if n, ok := wt.(tftp.IncomingTransfer).Size(); ok {
+		// 	fmt.Printf("Transfer size: %d\n", n)
+		// }
+		n, err := wt.WriteTo(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Close()
+		d := time.Now().Sub(ts)
+		fmt.Printf("GET %s/%s as %s : %d bytes received at %s/s\n",
+			Server, remoteFile, localFile,
+			n, prettyByteSize(float64(n)/(d.Seconds())))
+		file.Close()
+
+	default:
+		flag.Usage()
+	}
+
+}
+
+func prettyByteSize(bf float64) string {
+	for _, unit := range []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"} {
+		if math.Abs(bf) < 1024.0 {
+			return fmt.Sprintf("%3.1f%sB", bf, unit)
+		}
+		bf /= 1024.0
+	}
+	return fmt.Sprintf("%.1fYiB", bf)
+}
--- tftpd/main.go
+++ tftpd/main.go
@@ -19,39 +19,63 @@ func readHandler(filename string, rf io.ReaderFrom) error {
 
 	// Set transfer size before calling ReadFrom.
 	rf.(tftp.OutgoingTransfer).SetSize(8 * 1024)
+	// Set cisco "ip tftp blocksize 8192" (and allow fragmented udp packets)
+
+	// s.SetBlockSize(65456)
+	// https://github.com/pin/tftp/issues/41
+
+	raddr := rf.(tftp.OutgoingTransfer).RemoteAddr()
 
 	n, err := rf.ReadFrom(file)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		return err
 	}
-	fmt.Printf("%d bytes sent\n", n)
+
+	// fmt.Printf("%s : %d bytes sent from %s\n", filename, n, raddr.String())
+	fmt.Printf("%s: %d bytes sent to %s.\n", filename, n, raddr.IP.String())
+
 	return nil
 }
 
 // writeHandler is called when client starts file upload to server
 func writeHandler(filename string, wt io.WriterTo) error {
-	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		return err
 	}
+
 	n, err := wt.WriteTo(file)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		return err
 	}
-	fmt.Printf("%d bytes received\n", n)
+	// fmt.Printf("%s : %d bytes received from %s\n", filename, n, raddr.String())
+	fmt.Printf("%s: %d bytes received.\n", filename, n)
+
 	return nil
 }
 
 func main() {
 	// use nil in place of handler to disable read or write operations
 	s := tftp.NewServer(readHandler, writeHandler)
-	s.SetTimeout(5 * time.Second)  // optional
-	err := s.ListenAndServe(":69") // blocks until s.Shutdown() is called
+	s.SetBackoff(func(attempts int) time.Duration { // retransmit unacknowledged
+		return time.Duration(attempts) * time.Second
+	})
+	// s.SetBackoff(func(int) time.Duration { return 0 }) // No need for retries ?
+	// s.SetTimeout(5 * time.Second) // optional
+	err := s.ListenAndServe(getenv("DOCKER_TFTP_PORT", ":69"))
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "server: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
 }
