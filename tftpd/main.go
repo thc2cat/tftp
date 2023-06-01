2712f0514b3b2a20e647a2b4531bb0aff915b062
thc <thc2cat@gmail.com>
2023-06-01 10:01:40 +0200

    v0.3

--- tftpd/main.go
+++ tftpd/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"time"
 
 	// tftp "projects/pkg/mod/github.com/pin/tftp/v3"
 
 	tftp "github.com/pin/tftp/v3"
+)
+
+var (
+	setblocksize bool
 )
 
 // readHandler is called when client starts file download from server
@@ -20,7 +25,7 @@ func readHandler(filename string, rf io.ReaderFrom) error {
 	}
 
 	// Set transfer size before calling ReadFrom.
-	if len(getenv("TFTP_LOW_BLOCKSIZE", "")) == 0 {
+	if setblocksize {
 		rf.(tftp.OutgoingTransfer).SetSize(8 * 1024)
 	}
 	// Set cisco "ip tftp blocksize 8192" (and allow fragmented udp packets)
@@ -28,12 +33,18 @@ func readHandler(filename string, rf io.ReaderFrom) error {
 	// https://github.com/pin/tftp/issues/41
 
 	raddr := rf.(tftp.OutgoingTransfer).RemoteAddr()
+	ts := time.Now()
 	n, err := rf.ReadFrom(file)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		return err
 	}
-	fmt.Printf("Sent file %s (%d bytes) to %s\n", filename, n, raddr.IP.String())
+	file.Close()
+	d := time.Now().Sub(ts)
+	fmt.Printf("Sent file %s (%d bytes at %s/s) to %s\n",
+		filename, n,
+		prettyByteSize(float64(n)/(d.Seconds())),
+		raddr.IP.String())
 	return nil
 }
 
@@ -44,17 +55,26 @@ func writeHandler(filename string, wt io.WriterTo) error {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		return err
 	}
+	raddr := wt.(tftp.IncomingTransfer).RemoteAddr()
+	ts := time.Now()
 	n, err := wt.WriteTo(file)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		return err
 	}
-	raddr := wt.(tftp.IncomingTransfer).RemoteAddr()
-	fmt.Printf("Received file %s (%d bytes) from %s\n", filename, n, raddr.IP.String())
+	file.Close()
+	d := time.Now().Sub(ts)
+	fmt.Printf("Received file %s (%d bytes at %s/s) from %s\n",
+		filename, n,
+		prettyByteSize(float64(n)/(d.Seconds())),
+		raddr.IP.String())
 	return nil
 }
 
 func main() {
+	// If you cant set larger blocksize
+	setblocksize = (len(getenv("TFTP_DONTSET_BLOCKSIZE", "")) == 0)
+
 	// use nil in place of handler to disable read or write operations
 	s := tftp.NewServer(readHandler, writeHandler)
 	s.SetBackoff(func(attempts int) time.Duration { // retransmit unacknowledged
@@ -75,4 +95,14 @@ func getenv(key, fallback string) string {
 		return fallback
 	}
 	return value
+}
+
+func prettyByteSize(bf float64) string {
+	for _, unit := range []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"} {
+		if math.Abs(bf) < 1024.0 {
+			return fmt.Sprintf("%3.1f%sB", bf, unit)
+		}
+		bf /= 1024.0
+	}
+	return fmt.Sprintf("%.1fYiB", bf)
 }
